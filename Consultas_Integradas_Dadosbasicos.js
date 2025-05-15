56fd239a09c37a8f448449b33145a782f4a4c655
DP3D3733 <caleberuivo@gmail.com>
2025-05-15 11:35:53 -0300

    Update Consultas_Integradas_Dadosbasicos.js

--- Consultas_Integradas_Dadosbasicos.js
+++ Consultas_Integradas_Dadosbasicos.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Consultas_Integradas_Dadosbasicos
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @updateURL    https://raw.githubusercontent.com/DP3D3733/XCAD/main/Consultas_Integradas_Dadosbasicos.js
 // @downloadURL  https://raw.githubusercontent.com/DP3D3733/XCAD/main/Consultas_Integradas_Dadosbasicos.js
 // @description  Módulo de Consulta Integradas do XCAD
