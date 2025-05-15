f413b4c862e119860ba1a127a66ea128c4f51261
DP3D3733 <caleberuivo@gmail.com>
2025-05-15 11:35:18 -0300

    Update Cad_Ocorrencias.js

--- Cad_Ocorrencias.js
+++ Cad_Ocorrencias.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cad Ocorrencias
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @updateURL    https://raw.githubusercontent.com/DP3D3733/XCAD/main/Cad_Ocorrencias.js
 // @downloadURL  https://raw.githubusercontent.com/DP3D3733/XCAD/main/Cad_Ocorrencias.js
 // @description  Automações no CAD Ocorrências
