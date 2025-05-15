d928c6b934c0d9a15686a57c14804ada2226e129
DP3D3733 <caleberuivo@gmail.com>
2025-05-15 07:42:13 -0300

    Create user.js

---
+++ user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Exemplo do GitHub
+// @namespace    https://github.com/seunome
+// @version      1.0
+// @description  Script hospedado no GitHub
+// @author       Você
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/SEUUSUARIO/REPO/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/SEUUSUARIO/REPO/main/script.user.js
+// @grant        none
+// ==/UserScript==
+
+console.log("Script carregado do GitHub!");
