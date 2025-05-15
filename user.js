76e928c0b4072fa94e694663f62ae14d8456a589
DP3D3733 <caleberuivo@gmail.com>
2025-05-15 07:51:38 -0300

    Update user.js

--- user.js
+++ user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Exemplo do GitHub
 // @namespace    https://github.com/seunome
-// @version      1.0
+// @version      1.1
 // @description  Script hospedado no GitHub
 // @author       Você
 // @match        *://*/*
-// @updateURL    https://raw.githubusercontent.com/DP3D3733/XCAD/main/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/DP3D3733/XCAD/main/script.user.js
+// @updateURL    https://raw.githubusercontent.com/DP3D3733/XCAD/main/user.js
+// @downloadURL  https://raw.githubusercontent.com/DP3D3733/XCAD/main/user.js
 // @grant        none
 // ==/UserScript==
 
-console.log("Script carregado do GitHub!");
+console.log("Script carregado do GitHub! Duken");
